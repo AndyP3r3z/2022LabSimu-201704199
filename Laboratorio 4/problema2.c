c10959436de77c0761dbf838dfedf64419656923
Andy Pérez <andy.perez.amado@gmail.com>
2022-05-07 21:36:55 -0600

    EDIT: Segundo problema terminado.

--- Laboratorio 4/problema2.c
+++ Laboratorio 4/problema2.c
@@ -1,1 +1,50 @@
-// gcc -o problema2.out problema2.c -lm
+// COMPILAR: gcc -o problema2.out problema2.c
+// Librería
+#include <stdio.h>
+// Definir variables
+int v[5];
+// Prototipar funciones
+void fillVector();
+void sort();
+void printVector();
+
+// Inicio del Main
+int main(int argc, char const *argv[]) {
+  fillVector(); // Llenamos el vector
+  printVector();
+  sort(); // Luego lo ordenamos
+  printVector(); // Por último, lo imprimimos
+  return 0;
+}
+
+// Llenar el vector
+void fillVector(){
+  int n = 0;
+  for (int i = 0; i < 5; i++) {
+    printf("Ingrese un número entero.\n");
+    scanf("%d", &n);
+    v[i] = n;
+    }
+  }
+
+// Ordenar el vector
+void sort(){
+  for (int i = 0; i < 4; i++) {
+    for (int j = 0; j < 4-i; j++) {
+      if (v[j] > v[j+1]) {
+        int aux = v[j];
+        v[j] = v[j+1];
+        v[j+1] = aux;
+      }
+    }
+  }
+}
+
+// Imprimir el vector
+void printVector(){
+  puts("El vector ordenado es:");
+  for (int i = 0; i < 5; i++) {
+    printf("%d ", v[i]);
+  }
+  puts("");
+}
