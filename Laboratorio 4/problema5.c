cf436b13af51e37ce69944300c7e0c1cc4c672f3
Andy Pérez <andy.perez.amado@gmail.com>
2022-05-08 20:23:25 -0600

    EDIT: Quinto problema terminado.

--- Laboratorio 4/problema5.c
+++ Laboratorio 4/problema5.c
@@ -1,1 +1,25 @@
-// gcc -o problema6.out problema5.c -lm
+// gcc -o problema6.out problema5.c
+// Librerías.
+#include <stdio.h>
+// prototipar funciones.
+int factorial(int n);
+
+// Función principal.
+int main(int argc, char const *argv[]) {
+  int a = 1; // Número que se pedirá, es 1 por defecto.
+  puts("Ingrese un número entero positivo:");
+  scanf("%d", &a); // Guardamos el número.
+  printf("%d! = %d\n", a, factorial(a)); // Devolvemos su factorial.
+  return 0;
+}
+
+// Función recursiva.
+int factorial(int n){
+  if (n > 1) {
+    return n*factorial(n-1); // Si es mayor a 1, que lo multiplique por el anterior.
+  } else if (n == 1) {
+    return 1; // Límite inferior.
+  } else {
+    return 0; // Por si algo sale mal.
+  }
+}
